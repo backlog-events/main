d28abb8e51141a25a7d2261d80e273cbdd7e0c72
Pavel Voronin <pavel.voronin@maximumtest.ru>
2019-03-30 20:05:56 +0300

    load more Roboto

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,900&amp;subset=cyrillic-ext' }
     ]
   },
   css: [
